be51c1b4af5f393faf3c704e05f36e02371aea0f
Vasiliev Sergei <sergei.binp@gmail.com>
2023-05-19 01:03:55 +0500

    3rd commit: second type of methods version v2.0

--- practicies/Program.cs
+++ practicies/Program.cs
@@ -13,3 +13,15 @@ void Method2(string Name)
 string nameOfUser = "Serjio";
 Method2(nameOfUser);
 
+// Функция 2 типа, v2.0
+void Method2_2(string Name, int num)
+{
+    int count = 0;
+    while (num != count)
+    {
+        Console.WriteLine("Получите посылку, " + Name);
+        count++;
+    }
+}
+string txt = "Serjio";
+Method2_2(txt, 4);
