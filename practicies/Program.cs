1c5f723ba3930bf92bd9e5f98c6966e779ed1596
Vasiliev Sergei <sergei.binp@gmail.com>
2023-05-19 00:52:22 +0500

    2nd commit: second type of methods

--- practicies/Program.cs
+++ practicies/Program.cs
@@ -4,3 +4,12 @@ void Method1()
     Console.WriteLine("Hello, Serjio!");
 }
 Method1();
+
+// Функция 2 типа
+void Method2(string Name)
+{
+    Console.WriteLine("Иди погуляй, шаги намотай, " + Name);
+}
+string nameOfUser = "Serjio";
+Method2(nameOfUser);
+
