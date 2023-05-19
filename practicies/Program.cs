7a008d123d5ae539babca752a8328ad88f753104
Vasiliev Sergei <sergei.binp@gmail.com>
2023-05-19 22:23:06 +0500

    9th commit: task about array sorting

--- practicies/Program.cs
+++ practicies/Program.cs
@@ -85,28 +85,60 @@
 //****************************************
 // РАБОТА С ТЕКСТАМИ
 // Дан текст. Нужно все пробелы заменить черточками, буквы "к" - буквами "К", а буквы "с" - буквами "С".
-Console.WriteLine("ПРИВЕТ!");
-string text = "Однажды в студёную зимнюю пору"
-            + "Лошадка пипиркой примёрзла к забору!"
-            + "Она и лягалась, она и брыкалась!"
-            + "Лошадка ушла, а пипирка - осталась...";
+// Console.WriteLine("ПРИВЕТ!");
+// string text = "Однажды в студёную зимнюю пору"
+//             + "Лошадка пипиркой примёрзла к забору!"
+//             + "Она и лягалась, она и брыкалась!"
+//             + "Лошадка ушла, а пипирка - осталась...";
 
-string Replace(string text, char oldValue, char newValue)
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if (text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+//     return result;
+// }
+// string newText = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'с', 'С');
+// Console.WriteLine(newText);
+
+// Сортировка массива по возрастанию
+int[] arr = {1, 5, 7, 2, 8, 4, 1, 3, 6, 1, 1};
+
+void PrintArray(int[] array)
 {
-    string result = String.Empty;
-    int length = text.Length;
-    for (int i = 0; i < length; i++)
+    int count = array.Length;
+    for(int i = 0; i < count; i++)
     {
-        if (text[i] == oldValue) result = result + $"{newValue}";
-        else result = result + $"{text[i]}";
+        Console.Write($"{array[i]} ");
     }
-    return result;
-}
-string newText = Replace(text, ' ', '|');
-Console.WriteLine(newText);
-Console.WriteLine();
-newText = Replace(newText, 'к', 'К');
-Console.WriteLine(newText);
-Console.WriteLine();
-newText = Replace(newText, 'с', 'С');
-Console.WriteLine(newText);
+    Console.WriteLine();
+} // Конец метода PrintArray
+
+void SelectionSort(int[] array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+} // Конец метода SelectionSort
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
