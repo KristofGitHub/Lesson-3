23f8d2bee8adad57482ead3f5840729250644619
Vasiliev Sergei <sergei.binp@gmail.com>
2023-05-19 21:44:41 +0500

    8th commit: task about text replacing

--- practicies/Program.cs
+++ practicies/Program.cs
@@ -81,3 +81,32 @@
 //     }
 //     i = 0;
 // };
+
+//****************************************
+// РАБОТА С ТЕКСТАМИ
+// Дан текст. Нужно все пробелы заменить черточками, буквы "к" - буквами "К", а буквы "с" - буквами "С".
+Console.WriteLine("ПРИВЕТ!");
+string text = "Однажды в студёную зимнюю пору"
+            + "Лошадка пипиркой примёрзла к забору!"
+            + "Она и лягалась, она и брыкалась!"
+            + "Лошадка ушла, а пипирка - осталась...";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'с', 'С');
+Console.WriteLine(newText);
